fe98936f068657d0920183e33b62bd015e9a4527
Sonakshi <sonakshishukla1501@gmail.com>
2022-02-15 14:06:08 +0530

    child component

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-app>
+    <v-main>
+      <h1>User Table:Child 1</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto delectus libero cupiditate, tempora debitis nobis temporibus repellendus nam atque eveniet saepe quos ea! Libero non odit odio qui itaque alias!</p>
+      <v-table>
+
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-table>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ],
+      }
+    },
+  }
+</script>
